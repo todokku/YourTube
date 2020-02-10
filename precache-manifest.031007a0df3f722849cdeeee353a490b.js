61d6c252c17b4c24d99cbd106e3d0aaf5d8d68b8
Tien Loc Le <eddieGong2604@users.noreply.github.com>
2020-02-11 06:15:24 +1100

    Updates

---
+++ precache-manifest.031007a0df3f722849cdeeee353a490b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f387dcdad0e13de299ffa4a198e17e67",
+    "url": "/YourTube/index.html"
+  },
+  {
+    "revision": "87bc41edb98f059d9d00",
+    "url": "/YourTube/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "755d3fe53812e150e51c",
+    "url": "/YourTube/static/css/main.b4160578.chunk.css"
+  },
+  {
+    "revision": "87bc41edb98f059d9d00",
+    "url": "/YourTube/static/js/2.ed8b16ee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/YourTube/static/js/2.ed8b16ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "755d3fe53812e150e51c",
+    "url": "/YourTube/static/js/main.05a3efa6.chunk.js"
+  },
+  {
+    "revision": "ef00b787383930cdef40",
+    "url": "/YourTube/static/js/runtime-main.38ad3b27.js"
+  }
+]);
